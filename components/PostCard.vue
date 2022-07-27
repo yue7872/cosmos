11d8a749e1837d2f4c6642ea814d400243807657
yue7872 <2258462872@qq.com>
2022-07-27 17:53:21 +0800

    style: 添加postcard样式 。

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,3 +1,25 @@
+<template>
+  <div
+    mb-20px
+    p-30px
+    border-rd-20px
+    min-h-200px
+    box-border
+    cursor-pointer
+    flex
+    justify-center
+    flex-col
+    class="posts-item-box"
+  >
+    <NuxtLink :to="`articles/${title}.vue`">
+      <div hover-color-red font-medium text-28px>{{ articleInfo.title }}</div>
+      <div>日期</div>
+      <div>分类</div>
+      <div color-coolGray>{{ articleInfo }}</div>
+      <div v-if="articleInfo.outline">摘要：{{ articleInfo.outline }}</div>
+    </NuxtLink>
+  </div>
+</template>
 <script setup lang="ts">
 const props = defineProps({
   post: {
@@ -9,14 +31,11 @@ const props = defineProps({
 const post: PostDetail = props.post;
 const { title, articleInfo } = post;
 </script>
-<template>
-  <div border-solid mb-10px>
-    <NuxtLink :to="`articles/${title}.vue`">
-      <div hover-color-red>{{ articleInfo.title }}</div>
-      <div>日期</div>
-      <div>分类</div>
-      <div color-coolGray>{{ articleInfo }}</div>
-      <div v-if="articleInfo.outline">摘要：{{ articleInfo.outline }}</div>
-    </NuxtLink>
-  </div>
-</template>
+<style lang="less">
+.posts-item-box {
+  box-shadow: 0 3px 8px 6px rgba(7, 17, 27, 0.06);
+  &:hover {
+    box-shadow: 0 3px 8px 6px rgba(7, 17, 27, 0.15);
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div max-w-1200px ma>
-    <NavBar />
+  <div max-w-1200px ma pl-32px pr-32px pb-100px>
+    <NavBar mb-80px />
     <slot></slot>
   </div>
 </template>
