67c90c67fb65f51b3b708c09f720d680f7159115
yue7872 <2258462872@qq.com>
2022-08-04 15:41:17 +0800

    feat: 新增打字机样式 。

--- composables/useTyper.ts
+++ composables/useTyper.ts
@@ -1,21 +1,22 @@
-export const useTyper = (text: string, speed: number) => {
+export const useTyper = (text: string, speed: number, noBack?: boolean) => {
   const typedText = ref("");
   const typing = ref(true);
-
   const interval = setInterval(() => {
     if (typedText.value.length < text.length) {
       typedText.value += text[typedText.value.length];
     } else {
       clearInterval(interval);
       // 回退
-      const intervalBack = setInterval(() => {
-        if (typedText.value.length > 0) {
-          typedText.value = typedText.value.slice(0, -1);
-        } else {
-          typing.value = false;
-          clearInterval(intervalBack);
-        }
-      }, speed);
+      const intervalBack = noBack
+        ? null
+        : setInterval(() => {
+            if (typedText.value.length > 0) {
+              typedText.value = typedText.value.slice(0, -1);
+            } else {
+              typing.value = false;
+              clearInterval(intervalBack);
+            }
+          }, speed);
     }
   }, speed);
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
-<script setup>
+<script setup lang="ts">
+import { Ref } from "vue";
+
 const route = useRoute();
 
 definePageMeta({
@@ -12,14 +14,23 @@ useHead({
 });
 const dailySentence = await useDailySentence();
 const { typedText, typing } = useTyper(dailySentence, 100);
-let myCustomText = "hahah";
-if (!typing) {
-  myCustomText = useTyper("花花婚你就能", 100).typedText;
-}
+let subtitle = reactive({
+  content: "",
+});
+
+watch(typing, (typing) => {
+  if (typing === false) {
+    const { typedText: myCustomText } = useTyper(
+      "学海无涯，回头是岸。",
+      100,
+      true
+    );
+    subtitle.content = myCustomText;
+  }
+});
 </script>
 <template>
   <div>
-    <div>{{ typing ? typedText : myCustomText }}</div>
-    <div>{{ myCustomText }}</div>
+    <div>{{ typing ? typedText : subtitle.content }}</div>
   </div>
 </template>
